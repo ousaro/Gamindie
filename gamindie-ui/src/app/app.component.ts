a4a09f191c8456c4c7b937a5dd891f7e847c494c
ousaro <oussamaouldhila4@gmail.com>
2024-12-26 00:33:49 +0100

    adding the home layout

--- gamindie-ui/src/app/app.component.ts
+++ gamindie-ui/src/app/app.component.ts
@@ -3,13 +3,17 @@ import { RouterOutlet } from '@angular/router';
 import { DarkModeService } from './core/services/theme/dark-mode.service';
 import { CommonModule } from '@angular/common';
 import { TokenService } from './core/services/token/token.service';
+import { TopBarComponent } from "./core/components/top-bar/top-bar.component";
+import { LeftBarComponent } from "./core/components/left-bar/left-bar.component";
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     CommonModule,
-  ],
+    TopBarComponent,
+    LeftBarComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ gamindie-ui/src/app/core/components/left-bar/left-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-left-bar',
+  imports: [],
+  templateUrl: './left-bar.component.html',
+  styleUrl: './left-bar.component.scss'
+})
+export class LeftBarComponent {
+
+}
